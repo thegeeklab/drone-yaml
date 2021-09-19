b1275997d76cc2365c46117b189a3ee819e65910
Robert Kaussow <mail@thegeeklab.de>
2021-09-19 13:49:54 +0200

    refactor: remove unused components

--- yaml/pretty/util_test.go
+++ yaml/pretty/util_test.go
@@ -1,16 +1,5 @@
-// Copyright the Drone Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright (c), the Drone Authors.
+// Copyright (c) 2021, Robert Kaussow <mail@thegeeklab.de>
 
 package pretty
 
@@ -22,7 +11,7 @@ import (
 )
 
 func TestQuoted(t *testing.T) {
-	tests := []struct{
+	tests := []struct {
 		before, after string
 	}{
 		{"", `""`},
@@ -102,8 +91,3 @@ var testChunk = []string{
 	"N2RiNTIwZjg0NWQwYjcyYjE3MmFmZDIyYzg3NTQ1N2YyYzgxODhjYjJmNDhhOTFj",
 	"ZjdhMzA0YjEzYWFlMmYxMTIwMmEyM2Q1YjQ5Yjg2ZmMK",
 }
-
-var testScalar = `>
-  ZDllMjFjZDg3Zjk0ZWFjZDRhMjdhMTA1ZDQ1OTVkYTA1ODBjMTk0ZWVlZjQyNmU4
-  N2RiNTIwZjg0NWQwYjcyYjE3MmFmZDIyYzg3NTQ1N2YyYzgxODhjYjJmNDhhOTFj
-  ZjdhMzA0YjEzYWFlMmYxMTIwMmEyM2Q1YjQ5Yjg2ZmMK`
